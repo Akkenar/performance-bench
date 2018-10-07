1e5ffede3f5a7f4fdbbfa3f61e797027799cea5d
Pierre-Dominique Putallaz <pierre-dominique.putallaz@nestle.com>
2018-10-07 20:00:59 +0200

    (angular) Fix routing

--- projects/angular-demo/src/app/header/header.module.ts
+++ projects/angular-demo/src/app/header/header.module.ts
@@ -6,7 +6,6 @@ import {
   MatToolbarModule,
 } from '@angular/material';
 import { RouterModule } from '@angular/router';
-import { routes } from '../app-routing.module';
 import { UserModule } from '../core/user/user.module';
 import { SharedModule } from '../shared.module';
 import { HeaderComponent } from './header.component';
@@ -14,7 +13,7 @@ import { HeaderComponent } from './header.component';
 @NgModule({
   declarations: [HeaderComponent],
   imports: [
-    RouterModule.forChild(routes),
+    RouterModule,
     SharedModule,
     MatMenuModule,
     MatButtonModule,

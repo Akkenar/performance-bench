3ef752c7119981fab236e4ef68888b04bd10618c
Pierre-Dominique Putallaz <pierre-dominique.putallaz@nestle.com>
2018-10-14 10:01:01 +0200

    (react) Add simple header with routing

--- projects/react-demo/src/App.js
+++ projects/react-demo/src/App.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Header from './header/Header';
+
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import LoginPage from './login-page/LoginPage';
+import HomePage from './home-page/HomePage';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <Header />
+          </header>
+          <main>
+            <Route exact path="/" component={HomePage} />
+            <Route path={'/login'} component={LoginPage} />
+            <Route path={'/home'} component={HomePage} />
+          </main>
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ projects/react-demo/src/header/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import Book from '@material-ui/icons/Book';
+
+import './Header.css';
+
+const Header = props => {
+  return (
+    <AppBar>
+      <Toolbar>
+        <IconButton aria-label="Menu">
+          <Book />
+        </IconButton>
+        <Typography variant="h6" color="inherit">
+          React Performance Benchmark
+        </Typography>
+        <div className="Header--menu-container">
+          <Button component={Link} to="/home">
+            Home
+          </Button>
+          <Button component={Link} to="/login">
+            Login
+          </Button>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
